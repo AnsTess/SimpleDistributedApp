507820754b2205fc60bafcffbee4ef1371202be0
Kirill Berezin <berezk7@gmail.com>
2023-12-18 01:18:59 +0400

    Добавьте файлы проекта.

---
+++ SimpleDistributedApp/ClientSide.cpp
@@ -0,0 +1,37 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WSADATA wsaData;
+    SOCKET clientSocket;
+    SOCKADDR_IN client;
+
+    // Инициализация Winsock
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    // Создание сокета
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Подключение к серверу
+    client.sin_family = AF_INET;
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+    client.sin_port = htons(1111);
+    connect(clientSocket, (SOCKADDR*)&client, sizeof(client));
+    std::cout << "Подключено к серверу" << std::endl;
+
+    // Отправка и прием сообщений
+    const char* message = "Привет от клиента!";
+    send(clientSocket, message, strlen(message), 0);
+
+    char buffer[1024];
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    std::cout << "Получено: " << buffer << std::endl;
+
+    // Закрытие сокета
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ SimpleDistributedApp/ServerSide.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WSADATA wsaData;
+    SOCKET serverSocket, clientSocket;
+    SOCKADDR_IN server, client;
+    int clientSize = sizeof(client);
+
+    // Инициализация Winsock
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    // Создание сокета
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Привязка сокета
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(1111);
+    bind(serverSocket, (SOCKADDR*)&server, sizeof(server));
+
+    // Ожидание входящих соединений
+    listen(serverSocket, 0);
+    std::cout << "Сервер слушает..." << std::endl;
+
+    // Принятие входящих соединений
+    clientSocket = accept(serverSocket, (SOCKADDR*)&client, &clientSize);
+    std::cout << "Клиент подключен" << std::endl;
+
+    // Прием и отправка сообщений
+    char buffer[1024];
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    std::cout << "Получено: " << buffer << std::endl;
+
+    const char* message = "Привет от сервера!";
+    send(clientSocket, message, strlen(message), 0);
+
+    // Закрытие сокета
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
